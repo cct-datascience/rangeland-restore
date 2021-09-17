5fa85b250536e80094a056aa3858221a36bb84d1
Jessica Guo <jessicaguo@email.arizona.edu>
2021-09-16 21:10:27 -0700

    Begin adding greenstrip model figure

---
+++ scripts/03_count_figures.R
@@ -0,0 +1,72 @@
+# Figures script for biomass data
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(ggthemes)
+
+### ALL
+# Read in raw data
+load("../../../cleaned_data/count_all.Rdata") # count_all
+raw_dat <- dat %>%
+  filter(quadrat < 10000) %>%
+  mutate(BRTE_count_m2 = BRTE/quadrat*100*100)
+
+# Load coda and coda.rep
+load(file = "../models/count/all/coda/coda.Rdata") # coda.out
+# Summarize coda
+# Note that tidyMCMC drops the deviance estimate
+sum_out <- broom.mixed::tidyMCMC(coda.out, conf.int = TRUE, 
+                                 conf.level = 0.95) %>% 
+  rename(param = term, mean = estimate, sd = std.error, 
+         pc2.5 = conf.low, pc97.5 = conf.high) %>% 
+  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE))
+
+# Select and organize group means
+model_dat <- sum_out %>%
+  filter(grepl("m\\.", param)) %>%
+  mutate(grazing = case_when(grepl("ungrazed", param) ~ "ungrazed",
+                             grepl("fall", param) ~ "fall",
+                             grepl("spring", param) ~ "spring"),
+         fuelbreak = case_when(grepl("control", param) ~ "control",
+                         grepl("herbicide", param) ~ "herbicide",
+                         grepl("greenstrip", param) ~ "greenstrip"),
+         grazing = factor(grazing, levels = c("ungrazed", "fall", "spring")),
+         fuelbreak = factor(fuelbreak, levels = c("control", "herbicide", "greenstrip")))
+
+
+fig2 <- ggplot() +
+  geom_point(data = raw_dat, aes(x = fuelbreak, y = BRTE_count_m2, color = grazing),
+             position = position_jitterdodge(dodge.width = 0.5, jitter.width = 0.2),
+             alpha = 0.2) +
+  geom_pointrange(data = model_dat, aes(x = fuelbreak, y = mean*100,
+                                        ymin = pc2.5*100,
+                                        ymax = pc97.5*100, 
+                                        color = grazing),
+                  shape = 15,
+                  size = 0.75,
+                  position = position_dodge(width = 0.5)) +
+  scale_y_continuous(expression(paste("BRTE ", m^-2))) +
+  scale_color_canva(palette = "Surf and turf") +
+  theme_bw(12) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 12),
+        legend.position = c(.9, .85),
+        legend.title = element_blank())
+
+jpeg(filename = "../plots/Fig2_count_all.jpg",
+     height = 4, width = 6,
+     units = "in", res = 600)
+print(fig2)
+dev.off()
+
+### ALL
+# Read in raw data
+load("../../../cleaned_data/count_all.Rdata") # count_all
+raw_dat <- dat %>%
+  filter(quadrat < 10000) %>%
+  mutate(BRTE_count_m2 = BRTE/quadrat*100*100)
+
+# Load coda and coda.rep
+load(file = "../models/count/all/coda/coda.Rdata") # coda.out
