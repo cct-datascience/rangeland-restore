02776d85c08d0f1f3aa8567837a7c03b9e0be353
Jessica Guo <jessicaguo@email.arizona.edu>
2021-12-17 15:38:52 -0700

    Change to zib model and plots

--- models/cover/forbs/greenstrip/Forbs_cover.R
+++ models/cover/forbs/greenstrip/Forbs_cover.R
@@ -34,21 +34,30 @@ ggplot(dat, aes(x = spatial, y = forbs, color = block)) +
   facet_grid(rows = vars(grazing),
              cols = vars(block))
 
+summary(dat$forbs)
+# No zeros
+
 # model matrix
 X <- model.matrix( ~ (spatial + seed_rate + seed_coat + grazing)^2, data = dat) 
 colnames(X)
 
-# split the data into discrete and continuous components
-y.temp <- with(dat, ifelse(forbs == 1 | forbs == 0, 
+# split the data into 0 and continuous components
+y.temp <- with(dat, ifelse(forbs == 0, 
                            forbs, NA))
-y.discrete <- ifelse(is.na(y.temp), 0, 1)
-sum(y.discrete)
-# no discrete observations in this dataset
-# only continuous response + predictors
+y.0 <- ifelse(is.na(y.temp), 0, 1)
+
+# group continuous response + predictors
+which.cont <- which(y.0 == 0)
+y.c <- dat$forbs[which.cont]
+x.c <- X[which.cont,]
+n.cont <- length(y.c)
+block.c <- as.numeric(dat$block)[which.cont]
 
 # Assemble model inputs
 datlist <- list(N = nrow(dat),
-                y = dat$forbs,
+                y.0 = y.0,
+                n.cont = n.cont,
+                y.c = y.c,
                 mono = X[,2],
                 high = X[,3],
                 coated = X[,4],
@@ -63,9 +72,10 @@ datlist <- list(N = nrow(dat),
                 high_spring = X[,13],
                 coated_fall = X[,14],
                 coated_spring = X[,15],
-                block = as.numeric(dat$block),
+                block.c = block.c,
                 Nb = length(unique(dat$block)),
                 nL = ncol(X) - 1)
+str(datlist)
 
 # likely intercept value
 base <- dat %>%
@@ -83,7 +93,8 @@ inits <- function(){
   list(alpha = rnorm(1, 0, 10),
        beta = rnorm(ncol(X) - 1, 0, 10),
        tau = runif(1, 0, 1),
-       tau.eps = runif(1, 0, 0.1))
+       tau.eps = runif(1, 0, 0.1),
+       rho = runif(1, 0, 1))
 }
 initslist <- list(inits(), inits(), inits())
 
@@ -94,7 +105,7 @@ initslist <- list(append(saved.state[[2]][[1]], list(.RNG.name = array("base::Su
                   append(saved.state[[2]][[3]], list(.RNG.name = array("base::Mersenne-Twister"), .RNG.seed = array(18))))
 
 # model
-jm <- jags.model(file = "Forbs_cover_beta.jags",
+jm <- jags.model(file = "Forbs_cover_zib.jags",
                  inits = initslist,
                  n.chains = 3,
                  data = datlist)
@@ -102,7 +113,7 @@ jm <- jags.model(file = "Forbs_cover_beta.jags",
 
 # params to monitor
 params <- c("deviance", # evaluate fit
-            "alpha", "beta", # parameters
+            "rho", "alpha", "beta", # parameters
             "tau", "sig", "tau.eps", "sig.eps", # precision/variance terms
             "alpha.star", "eps.star", # identifiable intercept and random effects
             "int_Beta", "Diff_Beta", "diff_Beta", # monitored main and two-way treatment effects
@@ -121,9 +132,10 @@ coda.out <- coda.samples(jm, variable.names = params,
                          n.iter = 15000, thin = 5)
 
 # plot chains
-mcmcplot(coda.out, parms = c("deviance", "beta",
+mcmcplot(coda.out, parms = c("deviance", "rho", "beta", 
                              "alpha.star",  "eps.star", 
                              "sig", "sig.eps"))
+traplot(coda.out, parms = "rho")
 caterplot(coda.out, parms = "eps.star", reorder = FALSE)
 caterplot(coda.out, parms = "beta", reorder = FALSE)
 caterplot(coda.out, parms = "Diff_Beta", reorder = FALSE)
@@ -140,7 +152,7 @@ gel
 # If not converged, restart model from final iterations
 # newinits <-  initfind(coda.out)
 # newinits[[1]]
-# saved.state <- removevars(newinits, variables = c(1, 3, 5:33))
+# saved.state <- removevars(newinits, variables = c(1, 3, 5:31, 33:34))
 # saved.state[[1]]
 # save(saved.state, file = "inits/inits.Rdata")
 
@@ -148,7 +160,7 @@ save(coda.out, file = "coda/coda.Rdata")
 
 
 # Model fit
-params <- c("y.rep") #monitor replicated data
+params <- c("y.0.rep", "y.c.rep") #monitor replicated data
 coda.rep <- coda.samples(jm, variable.names = params,
                          n.iter = 15000, thin = 5)
 save(coda.rep, file = "coda/coda_rep.Rdata")
