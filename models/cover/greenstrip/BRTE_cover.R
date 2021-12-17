983ffe3d3a98cb57178fc4d11e92ed779dee38b9
Jessica Guo <jessicaguo@email.arizona.edu>
2021-12-17 13:49:25 -0700

    Update to zero-inflated beta, although replicated data could not be run

--- models/cover/greenstrip/BRTE_cover.R
+++ models/cover/greenstrip/BRTE_cover.R
@@ -32,7 +32,10 @@ str(dat)
 ggplot(dat, aes(x = grazing, y = BRTE, color = block)) +
   geom_point()
 
-ggplot(dat, aes(x = spatial, y = BRTE, color = block)) +
+# Check distribution of zeros
+dat %>%
+  filter(BRTE == 0) %>%
+ggplot(aes(x = spatial, y = BRTE, color = block)) +
   geom_point() +
   facet_wrap(~grazing)
 
@@ -40,18 +43,13 @@ ggplot(dat, aes(x = spatial, y = BRTE, color = block)) +
 X <- model.matrix( ~ (spatial + seed_rate + seed_coat + grazing)^2, data = dat) 
 colnames(X)
 
-# split the data into discrete and continuous components
-y.temp <- with(dat, ifelse(BRTE == 1 | BRTE == 0, BRTE, NA))
-y.discrete <- ifelse(is.na(y.temp), 0, 1)
-
-# group discrete response + predictors
-y.d <- y.temp[!is.na(y.temp)]
-x.d <- X[y.discrete == 1,]
-n.discrete <- length(y.d)
-block.d <- as.numeric(dat$block)[y.discrete == 1]
+# split the data into 0 and continuous components
+y.temp <- with(dat, ifelse(BRTE == 0, 
+                           BRTE, NA))
+y.0 <- ifelse(is.na(y.temp), 0, 1)
 
 # group continuous response + predictors
-which.cont <- which(y.discrete == 0)
+which.cont <- which(y.0 == 0)
 y.c <- dat$BRTE[which.cont]
 x.c <- X[which.cont,]
 n.cont <- length(y.c)
@@ -59,26 +57,9 @@ block.c <- as.numeric(dat$block)[which.cont]
 
 # Assemble model inputs
 datlist <- list(N = nrow(dat),
-                y.discrete = y.discrete,
-                n.discrete = n.discrete,
-                y.d = y.d, 
-                mono = x.d[,2],
-                high = x.d[,3],
-                coated = x.d[,4],
-                fall = x.d[,5],
-                spring = x.d[,6],
-                mono_high = x.d[,7],
-                mono_coated = x.d[,8],
-                mono_fall = x.d[,9],
-                mono_spring = x.d[,10],
-                high_coated = x.d[,11],
-                high_fall = x.d[,12],
-                high_spring = x.d[,13],
-                coated_fall = x.d[,14],
-                coated_spring = x.d[,15],
-                block.d = block.d,
+                y.0 = y.0,
                 n.cont = n.cont,
-                y.c = y.c,
+                y.c = y.c, 
                 mono2 = x.c[,2],
                 high2 = x.c[,3],
                 coated2 = x.c[,4],
@@ -96,6 +77,7 @@ datlist <- list(N = nrow(dat),
                 block.c = block.c,
                 Nb = length(unique(dat$block)),
                 nL = ncol(X) - 1)
+str(datlist)
 
 # likely intercept value
 base <- dat %>%
@@ -112,7 +94,7 @@ inits <- function(){
        beta = rnorm(ncol(X) - 1, 0, 10),
        tau = runif(1, 0, 1),
        tau.eps = runif(1, 0, 1),
-       psi = runif(1, 0, 1))
+       rho = runif(1, 0, 1))
 }
 initslist <- list(inits(), inits(), inits())
 
@@ -123,7 +105,7 @@ initslist <- list(append(saved.state[[2]][[1]], list(.RNG.name = array("base::Ma
                   append(saved.state[[2]][[3]], list(.RNG.name = array("base::Mersenne-Twister"), .RNG.seed = array(18))))
 
 # model
-jm <- jags.model(file = "BRTE_cover_zoib.jags",
+jm <- jags.model(file = "BRTE_cover_zib.jags",
                  inits = initslist,
                  n.chains = 3,
                  data = datlist)
@@ -131,7 +113,7 @@ jm <- jags.model(file = "BRTE_cover_zoib.jags",
 
 # params to monitor
 params <- c("deviance", # evaluate fit
-            "psi", "alpha", "beta", # parameters
+            "rho", "alpha", "beta", # parameters
             "tau", "sig", "tau.eps", "sig.eps", # precision/variance terms
             "alpha.star", "eps.star", # identifiable intercept and random effects
             "int_Beta", "Diff_Beta", "diff_Beta", # monitored main and two-way treatment effects
@@ -148,10 +130,10 @@ coda.out <- coda.samples(jm, variable.names = params,
                          n.iter = 15000, thin = 5)
 
 # plot chains
-mcmcplot(coda.out, parms = c("deviance", "psi", "beta",
+mcmcplot(coda.out, parms = c("deviance", "rho", "beta",
                              "alpha.star",  "eps.star", 
                              "sig", "sig.eps"))
-traplot(coda.out, parms = "psi")
+traplot(coda.out, parms = "rho")
 caterplot(coda.out, parms = "eps.star", reorder = FALSE)
 caterplot(coda.out, parms = "beta", reorder = FALSE)
 caterplot(coda.out, parms = "Diff_Beta", reorder = FALSE)
@@ -169,7 +151,7 @@ gel
 # # If not converged, restart model from final iterations
 # newinits <-  initfind(coda.out)
 # newinits[[1]]
-# saved.state <- removevars(newinits, variables = c(1, 3, 5:7, 9:10))
+# saved.state <- removevars(newinits, variables = c(1, 3, 5:31, 33:34))
 # saved.state[[1]]
 # save(saved.state, file = "inits/inits.Rdata")
 
@@ -177,7 +159,7 @@ save(coda.out, file = "coda/coda.Rdata")
 
 
 # Model fit
-params <- c("y.discrete.rep", "y.d.rep", "y.c.rep") #monitor replicated data
+params <- c("y.0.rep", "y.c.rep") #monitor replicated data
 coda.rep <- coda.samples(jm, variable.names = params,
                          n.iter = 15000, thin = 5)
 save(coda.rep, file = "coda/coda_rep.Rdata")
