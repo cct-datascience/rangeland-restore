f773316e736585301a30f7bca4d9dde7160c6caf
Jessica Guo <jessicaguo@email.arizona.edu>
2022-10-05 13:32:52 -0700

    Make multipanel plots

--- scripts/04_cover_figures_BRTE.R
+++ scripts/04_cover_figures_BRTE.R
@@ -1,13 +1,15 @@
 # Figures script for cover data at 3 levels
-library(coda)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggthemes)
+library(coda)
+library(broom.mixed)
+library(cowplot)
 
-### ALL
+##### LEVEL I #####
 # read in data
-load("../cleaned_data/cover_all.Rdata") # cover_all
+load("cleaned_data/cover_all.Rdata") # cover_all
 # convert to proportions
 raw_dat <- cover_all %>%
   mutate(BRTE = BRTE/100,
@@ -17,7 +19,7 @@ raw_dat <- cover_all %>%
 str(raw_dat)
 
 # Load coda and coda.rep
-load(file = "../models/cover/all/coda/coda.Rdata") # coda.out
+load(file = "models/cover/BRTE/all/coda/coda.Rdata") # coda.out
 
 # Summarize coda
 # Note that tidyMCMC drops the deviance estimate
@@ -25,7 +27,9 @@ sum_out <- broom.mixed::tidyMCMC(coda.out, conf.int = TRUE,
                                  conf.level = 0.95) %>% 
   rename(param = term, mean = estimate, sd = std.error, 
          pc2.5 = conf.low, pc97.5 = conf.high) %>% 
-  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE))
+  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE),
+         dir = case_when(sig == TRUE & mean > 0 ~ "pos",
+                         sig == TRUE & mean < 0 ~ "neg"))
 
 # Select and organize group means
 model_dat <- sum_out %>%
@@ -39,7 +43,8 @@ model_dat <- sum_out %>%
          grazing = factor(grazing, levels = c("ungrazed", "fall", "spring")),
          fuelbreak = factor(fuelbreak, levels = c("control", "herbicide", "greenstrip")))
 
-fig5 <- ggplot() +
+# raw + modeled
+fig5a <- ggplot() +
   geom_point(data = raw_dat, aes(x = fuelbreak, y = BRTE, color = grazing),
              position = position_jitterdodge(dodge.width = 0.5, jitter.width = 0.2),
              alpha = 0.2) +
@@ -62,8 +67,62 @@ fig5 <- ggplot() +
         legend.title = element_blank(),
         legend.background = element_rect(fill='transparent'))
 
-jpeg(filename = "../plots/Fig5_cover_all.jpg",
-     height = 4, width = 6,
+# main effect betas
+beta.labs2 <- c("fall", "spring", "herbicide", "seeding")
+beta.ind <- grep("Diff_Beta", sum_out$param)
+betas <- sum_out[beta.ind[1:length(beta.labs2)],]
+betas$param <- factor(betas$param, levels = betas$param)
+str(betas)
+fig5b <- ggplot() +
+  geom_pointrange(data = betas, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = subset(betas, sig == TRUE),
+             aes(x = param, y = min(pc2.5) - 0.01, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(betas$param)), labels = rev(beta.labs2)) +
+  scale_color_manual(values = c("goldenrod3", "forestgreen")) +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+# interaction betas
+beta.labs.ints <- c("fall:herbicide", "spring:herbicide", 
+                    "fall:seeding", "spring:seeding")
+beta.int.ind <- grep("diff_Beta", sum_out$param)
+beta.ints <- sum_out[beta.int.ind,]
+beta.ints$param <- factor(beta.ints$param, levels = beta.ints$param)
+str(beta.ints)
+fig5c <- ggplot() +
+  geom_pointrange(data = beta.ints, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = subset(beta.ints, sig == TRUE),
+             aes(x = param, y = min(pc2.5) - .01, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(beta.ints$param)), labels = rev(beta.labs.ints)) +
+  scale_color_manual(values = c("goldenrod3", "forestgreen")) +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+fig5_top <- plot_grid(fig5a, labels = "a")
+fig5_bottom <- plot_grid(fig5b, fig5c, ncol = 2, rel_widths = c(4, 5), labels = c("b", "c"))
+fig5 <- plot_grid(fig5_top, fig5_bottom, ncol = 1)
+
+
+jpeg(filename = "plots/Fig5_BRTE_cover_level1.jpg",
+     height = 6, width = 6,
      units = "in", res = 600)
 print(fig5)
 dev.off()
@@ -75,9 +134,9 @@ ttest <- ifelse(all[,2] < 0, 0, 1)
 mean(ttest)
 
 
-### GREENSTRIP
+##### LEVEL II #####
 # read in data
-load("../cleaned_data/cover_greenstrip.Rdata") # cover_greenstrip
+load("cleaned_data/cover_greenstrip.Rdata") # cover_greenstrip
 # convert to proportions
 raw_dat <- cover_greenstrip %>%
   mutate(BRTE = BRTE/100,
@@ -92,7 +151,7 @@ raw_dat <- cover_greenstrip %>%
          seed_coat = factor(seed_coat, levels = c("uncoated", "coated")))
 
 # Load coda and coda.rep
-load(file = "../models/cover/greenstrip/coda/coda.Rdata") # coda.out
+load(file = "models/cover/BRTE/greenstrip/coda/coda.Rdata") # coda.out
 
 # Summarize coda
 # Note that tidyMCMC drops the deviance estimate
@@ -100,7 +159,9 @@ sum_out <- broom.mixed::tidyMCMC(coda.out, conf.int = TRUE,
                                  conf.level = 0.95) %>% 
   rename(param = term, mean = estimate, sd = std.error, 
          pc2.5 = conf.low, pc97.5 = conf.high) %>% 
-  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE))
+  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE),
+         dir = case_when(sig == TRUE & mean > 0 ~ "pos",
+                         sig == TRUE & mean < 0 ~ "neg"))
 
 # Select and organize group means
 model_dat <- sum_out %>%
@@ -119,7 +180,8 @@ model_dat <- sum_out %>%
          seed_rate = factor(seed_rate, levels = c("low", "high")),
          seed_coat = factor(seed_coat, levels = c("uncoated", "coated")))
 
-fig6 <- ggplot() +
+# raw + modeled means
+fig6a <- ggplot() +
   geom_point(data = raw_dat, aes(x = spatial, y = BRTE, color = grazing),
              position = position_jitterdodge(dodge.width = 0.5, jitter.width = 0.2),
              alpha = 0.2) +
@@ -144,15 +206,71 @@ fig6 <- ggplot() +
         strip.background = element_rect(fill = "transparent"),
         legend.key.size = unit(.5, "lines"))
 
-jpeg(filename = "../plots/Fig6_cover_greenstrip.jpg",
-     height = 4, width = 6,
+# main effects betas
+beta.labs2 <- c("mono", "high", "coated", "fall", "spring")
+beta.ind <- grep("Diff_Beta", sum_out$param)
+betas <- sum_out[beta.ind[1:length(beta.labs2)],]
+betas$param <- factor(betas$param, levels = betas$param)
+str(betas)
+fig6b <- ggplot() +
+  geom_pointrange(data = betas, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = betas,
+             aes(x = param, y = min(pc2.5) - 0.01, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(betas$param)), labels = rev(beta.labs2)) +
+  scale_color_manual(values = c("forestgreen"),
+                     na.value = "transparent") +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+# interaction betas
+beta.labs.ints <- c("mono:high", "mono:coated", "mono:fall", "mono:spring",
+                    "high:coated", "high:fall", "high:spring", 
+                    "coated:fall", "coated:spring")
+beta.int.ind <- grep("diff_Beta", sum_out$param)
+beta.ints <- sum_out[beta.int.ind,]
+beta.ints$param <- factor(beta.ints$param, levels = beta.ints$param)
+str(beta.ints)
+fig6c <- ggplot() +
+  geom_pointrange(data = beta.ints, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = beta.ints,
+             aes(x = param, y = min(pc2.5) - 0.025, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(beta.ints$param)), labels = rev(beta.labs.ints)) +
+  scale_color_manual(values = c("forestgreen"),  na.value = "transparent") +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+fig6_top <- plot_grid(fig6a, labels = "a")
+fig6_bottom <- plot_grid(fig6b, fig6c, ncol = 2, rel_widths = c(4, 5), labels = c("b", "c"))
+fig6 <- plot_grid(fig6_top, fig6_bottom, ncol = 1)
+
+
+jpeg(filename = "plots/Fig6_BRTE_cover_level2.jpg",
+     height = 6, width = 6,
      units = "in", res = 600)
 print(fig6)
 dev.off()
 
-### MONO
+##### LEVEL III #####
 # Read in data
-load("../cleaned_data/cover_mono.Rdata") # cover_mono
+load("cleaned_data/cover_mono.Rdata") # cover_mono
 
 # Organize: remove largest quadrat and relevel species based on fig. 6b from Porensky et al. 2018
 raw_dat <- cover_mono %>%
@@ -168,7 +286,7 @@ raw_dat <- cover_mono %>%
          seed_coat = factor(seed_coat, levels = c("uncoated", "coated")))
 
 # Load coda and coda.rep
-load(file = "../models/cover/mono/coda/coda.Rdata") # coda.out
+load(file = "models/cover/BRTE/mono/coda/coda.Rdata") # coda.out
 
 # Summarize coda
 # Note that tidyMCMC drops the deviance estimate
@@ -176,7 +294,9 @@ sum_out <- broom.mixed::tidyMCMC(coda.out, conf.int = TRUE,
                                  conf.level = 0.95) %>% 
   rename(param = term, mean = estimate, sd = std.error, 
          pc2.5 = conf.low, pc97.5 = conf.high) %>% 
-  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE))
+  mutate(sig = if_else(pc2.5 * pc97.5 > 0, TRUE, FALSE),
+         dir = case_when(sig == TRUE & mean > 0 ~ "pos",
+                         sig == TRUE & mean < 0 ~ "neg"))
 
 # Select and organize group means
 model_dat <- sum_out %>%
@@ -198,7 +318,8 @@ model_dat <- sum_out %>%
          seed_rate = factor(seed_rate, levels = c("low", "high")),
          seed_coat = factor(seed_coat, levels = c("uncoated", "coated")))
 
-fig7 <- ggplot() +
+# raw + modeled means
+figS4a <- ggplot() +
   geom_point(data = raw_dat, aes(x = species, y = BRTE, color = grazing),
              position = position_jitterdodge(dodge.width = 0.5, jitter.width = 0.2),
              alpha = 0.2) +
@@ -223,8 +344,69 @@ fig7 <- ggplot() +
         strip.background = element_rect(fill = "transparent"),
         legend.key.size = unit(.5, "lines"))
 
-jpeg(filename = "../plots/Fig7_cover_mono.jpg",
-     height = 4, width = 6,
+# main effect betas
+beta.labs2 <- c("POSE", "POFE", "VUMI", "ELEL", "high", "fall", "spring", "coated")
+beta.ind <- grep("Diff_Beta", sum_out$param)
+betas <- sum_out[beta.ind[1:length(beta.labs2)],]
+betas$param <- factor(betas$param, levels = betas$param)
+str(betas)
+figS4b <- ggplot() +
+  geom_pointrange(data = betas, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = betas,
+             aes(x = param, y = min(pc2.5) - 0.01, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(betas$param)), labels = rev(beta.labs2)) +
+  scale_color_manual(values = c("forestgreen"), na.value = "transparent") +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+# interaction betas
+beta.labs.ints <- c("POSE:high", "POFE:high", "VUMI:high", "ELEL:high",
+                    "POSE:fall", "POFE:fall", "VUMI:fall", "ELEL:fall",
+                    "POSE:spring", "POFE:spring", "VUMI:spring", "ELEL:spring",
+                    "POSE:coated", "POFE:coated", "VUMI:coated", "ELEL:coated",
+                    "high:fall", "high:spring", "high:coated", "fall:coated", "spring:coated")
+
+beta.int.ind <- grep("diff_Beta", sum_out$param)
+beta.ints <- sum_out[beta.int.ind,]
+beta.ints$param <- factor(beta.ints$param, levels = beta.ints$param)
+str(beta.ints)
+figS4c <- ggplot() +
+  geom_pointrange(data = beta.ints, 
+                  aes(x = param, y = mean, ymin = pc2.5, ymax = pc97.5),
+                  size = 0.5) +
+  geom_point(data = beta.ints,
+             aes(x = param, y = min(pc2.5) - .01, col = as.factor(dir)),
+             shape = 8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  scale_y_continuous(expression(paste(Delta, " cheatgrass prop. cover"))) +
+  scale_x_discrete(limits = rev(levels(beta.ints$param)), labels = rev(beta.labs.ints)) +
+  scale_color_manual(values = c("forestgreen"), na.value = "transparent") +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  guides(color = "none")
+
+
+figS4_top <- plot_grid(figS4a, labels = "a")
+figS4_bottom <- plot_grid(figS4b, figS4c, ncol = 2, rel_widths = c(4, 5), labels = c("b", "c"))
+figS4 <- plot_grid(figS4_top, figS4_bottom, 
+                   rel_heights = c(4, 5), 
+                   ncol = 1)
+
+jpeg(filename = "plots/FigS4_BRTE_cover_level3.jpg",
+     height = 8, width = 6,
      units = "in", res = 600)
-print(fig7)
+print(figS4)
 dev.off()
+
